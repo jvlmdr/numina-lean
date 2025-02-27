8720f0e58106439c7925d8f42c9df6d6f84a57d1
Jack Valmadre <jack.valmadre@gmail.com>
2025-02-27 11:38:07 +1030

    Disable autoImplicit; solve algebra_94615

---
+++ Numina/algebra_94615.lean
@@ -0,0 +1,62 @@
+-- https://cloud.kili-technology.com/label/projects/label/cm7eniew000h5zz87966e5kv6?userID=cm7ln00x0cbs001at7mnncm9v
+
+import Mathlib
+
+/- Let f(x) = a * x^2 + b * x + c where a, b, c are integers.
+Suppose that f(1) = 0, 50 < f(7) < 60, 70 < f(8) < 80,
+and 5000k < f(100) < 5000(k + 1) for some integer k.
+What is k?
+(A) 1
+(B) 2
+(C) 3
+(D) 4
+(E) 5 -/
+
+theorem algebra_94615 (a b c : ℤ) (f : ℤ → ℤ) (hf : ∀ x, f x = a * x ^ 2 + b * x + c)
+    (h1 : f 1 = 0) (h2 : f 7 ∈ Set.Ioo 50 60) (h3 : f 8 ∈ Set.Ioo 70 80)
+    {k : ℤ} (h4 : f 100 ∈ Set.Ioo (5000 * k) (5000 * (k + 1))) :
+    k = 3 := by
+  simp only [hf, one_pow, mul_one, Int.reducePow] at h1 h2 h3
+  -- If r * x ∈ (a, b), what are the conditions to obtain x = k?
+  -- Require k - 1 < x < k + 1 and therefore r * (k - 1) < r * x < r * (k + 1).
+  -- This can be obtained from r * (k - 1) ≤ a and b ≤ r * (k + 1).
+  have h_eq_of_mem {r x a b k : ℤ} (h : r * x ∈ Set.Ioo a b) (hr : 0 < r)
+      (ha : r * (k - 1) ≤ a) (hb : b ≤ r * (k + 1)) : x = k := by
+    simp only [Set.mem_Ioo] at h
+    suffices k - 1 < x ∧ x < k + 1 by
+      symm
+      refine Int.le_antisymm_iff.mpr ?_
+      simpa [Int.sub_one_lt_iff, Int.lt_add_one_iff] using this
+    refine ⟨?_, ?_⟩
+    · exact Int.lt_of_mul_lt_mul_left (lt_of_le_of_lt ha h.1) hr.le
+    · exact Int.lt_of_mul_lt_mul_left (lt_of_lt_of_le h.2 hb) hr.le
+  -- Eliminate c from equations h2, h3.
+  -- Obtain equation for c to use later.
+  have hc : c = -(a + b) := by simpa [← eq_sub_iff_add_eq'] using h1
+  have h1 (u v) : a * u + b * v + c = a * (u - 1) + b * (v - 1) := by rw [hc]; ring
+  simp only [h1, Int.reduceSub] at h2 h3
+  -- Factor common term and divide on both sides.
+  have h2 : a * 8 + b = 9 := by
+    have h2 : 6 * (a * 8 + b) ∈ Set.Ioo 50 60 := by convert h2 using 1; ring
+    refine h_eq_of_mem h2 ?_ ?_ ?_ <;> norm_num
+  -- Factor common term and divide on both sides.
+  have h3 : a * 9 + b = 11 := by
+    have h3 : 7 * (a * 9 + b) ∈ Set.Ioo 70 80 := by convert h3 using 1; ring
+    refine h_eq_of_mem h3 ?_ ?_ ?_ <;> norm_num
+  -- Consider h3 - h2 in order to eliminate b.
+  have ha : a = 2 := by simpa [← mul_sub] using congrArg₂ (· - ·) h3 h2
+  -- Obtain value of b and c.
+  have hb : b = -7 := by simpa [ha, ← eq_sub_iff_add_eq'] using h2
+  have hc : c = 5 := by simpa [ha, hb] using hc
+  simp only [hf, ha, hb, hc, Int.reducePow, Int.reduceMul, Int.reduceNeg, Int.reduceAdd] at h4
+  -- 5000 * k < 19305 < 5000 * (k + 1)
+  -- k must be 3 since 15000 < 19305 < 20000.
+  refine Int.eq_iff_le_and_ge.mpr ⟨?_, ?_⟩
+  · suffices k < 4 from Int.lt_add_one_iff.mp this
+    refine Int.lt_of_mul_lt_mul_left ?_ (a := 5000) (by norm_num : 0 ≤ (5000 : ℤ))
+    refine h4.1.trans ?_
+    norm_num
+  · suffices 3 < k + 1 from (Int.add_le_add_iff_right 1).mp this
+    refine Int.lt_of_mul_lt_mul_left ?_ (a := 5000) (by norm_num : 0 ≤ (5000 : ℤ))
+    refine .trans ?_ h4.2
+    norm_num
--- lakefile.lean
+++ lakefile.lean
@@ -1,15 +1,39 @@
 import Lake
 open Lake DSL
 
-package «numina» where
-  -- Settings applied to both builds and interactive editing
-  leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
-  ]
-  -- add any additional package configuration options here
-
 require "leanprover-community" / "mathlib" @ git "v4.15.0"
+
+-- https://github.com/leanprover-community/mathlib4/blob/6b00c05/lakefile.lean
+abbrev mathlibOnlyLinters : Array LeanOption := #[
+  -- The `docPrime` linter is disabled: https://github.com/leanprover-community/mathlib4/issues/20560
+  ⟨`linter.docPrime, false⟩,
+  ⟨`linter.hashCommand, true⟩,
+  ⟨`linter.oldObtain, true,⟩,
+  ⟨`linter.refine, true⟩,
+  ⟨`linter.style.cdot, true⟩,
+  ⟨`linter.style.dollarSyntax, true⟩,
+  ⟨`linter.style.header, true⟩,
+  ⟨`linter.style.lambdaSyntax, true⟩,
+  ⟨`linter.style.longLine, true⟩,
+  ⟨`linter.style.longFile, .ofNat 1500⟩,
+  -- `latest_import.yml` uses this comment: if you edit it, make sure that the workflow still works
+  ⟨`linter.style.missingEnd, true⟩,
+  ⟨`linter.style.multiGoal, true⟩,
+  ⟨`linter.style.setOption, true⟩
+]
+
+/-- These options are passed as `leanOptions` to building mathlib, as well as the
+`Archive` and `Counterexamples`. (`tests` omits the first two options.) -/
+abbrev mathlibLeanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩
+  ] ++ -- options that are used in `lake build`
+    mathlibOnlyLinters.map fun s ↦ { s with name := `weak ++ s.name }
+
+package «numina» where
 
 @[default_target]
 lean_lib «Numina» where
-  -- add any library configuration options here
+  leanOptions := mathlibLeanOptions
+  -- Mathlib also enforces these linter options, which are not active by default.
+  moreServerOptions := mathlibOnlyLinters
