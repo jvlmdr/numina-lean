fc9130d5956b6aa4d1427231a9c93ae31bd4a126
Jack Valmadre <jack.valmadre@gmail.com>
2025-02-27 21:23:36 +1030

    Add to main lean file

--- Numina.lean
+++ Numina.lean
@@ -1,5 +1,4 @@
--- This module serves as the root of the `numina` library.
--- Import modules here that should be built as part of the library.
 import Numina.algebra_65302
 import Numina.algebra_94615
 import Numina.number_theory_93411
+import Numina.algebra_95071
