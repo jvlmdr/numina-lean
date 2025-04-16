c315f4cd1d5bfe645cb542d07dcb8e8cf3628712
Jack Valmadre <jack.valmadre@gmail.com>
2025-04-17 06:08:30 +0930

    problem_199764: complete

--- Numina/problem_199764.lean
+++ Numina/problem_199764.lean
@@ -7,24 +7,23 @@ open scoped List
 /- If you write all natural numbers $n$ with $111 \leq n \leq 999$ in any order consecutively,
 you will always get a sequence of digits that forms a number divisible by 37. -/
 
-theorem number_theory_199764 (l : List ℕ) (hl : l ~ List.Ico 111 1000) :
+theorem number_theory_199764 (l : List ℕ) (h_perm : l ~ List.Ico 111 1000) :
     37 ∣ Nat.ofDigits 10 (l.map (Nat.digits 10)).flatten := by
-  generalize hn : Nat.ofDigits 10 (l.map (Nat.digits 10)).flatten = n
-
-  have : Nat.ofDigits 10 (List.map (Nat.digits 10) l).flatten = Nat.ofDigits 1000 l := by
+  -- Since all numbers in the list have three digits, we can treat them as digits in base 1000.
+  suffices 37 ∣ Nat.ofDigits 1000 l by
+    convert this using 1
+    -- Use induction on the list to handle the concatenation.
     suffices ∀ t : List ℕ, (∀ x ∈ t, 100 ≤ x ∧ x < 1000) →
         Nat.ofDigits 10 (List.map (Nat.digits 10) t).flatten = Nat.ofDigits 1000 t by
-      refine this l ?_
-      refine fun x hx ↦ ?_
-      rw [hl.mem_iff, List.Ico.mem] at hx
+      refine this l fun x hx ↦ ?_
+      rw [h_perm.mem_iff, List.Ico.mem] at hx
       constructor <;> linarith
-
     intro t ht
     induction t with
     | nil => simp
     | cons x l IH =>
-      rw [List.map_cons, List.flatten_cons, Nat.ofDigits_append, Nat.ofDigits_cons]
-      rw [Nat.ofDigits_digits]
+      rw [Nat.ofDigits_cons]
+      rw [List.map_cons, List.flatten_cons, Nat.ofDigits_append, Nat.ofDigits_digits]
       congr 2
       · suffices (Nat.digits 10 x).length = 3 by simp [this]
         specialize ht x (List.mem_cons_self x l)
@@ -34,21 +33,23 @@ theorem number_theory_199764 (l : List ℕ) (hl : l ~ List.Ico 111 1000) :
       · refine IH fun y hy ↦ ?_
         exact ht y (List.mem_cons_of_mem x hy)
 
-  rw [← hn, this]
+  -- Since 1000 % 37 = 1, divisibility by 37 can be assessed from the sum of the digits.
   rw [Nat.dvd_iff_dvd_digits_sum 37 1000 (by norm_num)]
   rw [Nat.digits_ofDigits 1000 (by norm_num)]
   rotate_left
   · intro x hx
-    rw [hl.mem_iff, List.Ico.mem] at hx
+    rw [h_perm.mem_iff, List.Ico.mem] at hx
     exact hx.2
-  · intro hl_ne
-    have : 0 ∉ l := by simp [hl.mem_iff]
-    refine mt (fun h ↦ ?_) this
-    simpa [h] using List.getLast_mem hl_ne
+  · intro hl
+    suffices l.getLast hl ∈ List.Ico 111 1000 by
+      rw [List.Ico.mem] at this
+      linarith
+    simpa [h_perm.mem_iff] using List.getLast_mem hl
 
+  -- The sum of the arithmetic sequence {111, …, 999} divides 37 since 111 = 37 * 3.
   suffices l.sum = (111 + 999) * 889 / 2 from this ▸ Nat.dvd_mul_left 37 (15 * 889)
   calc _
-  _ = (List.Ico 111 1000).sum := hl.sum_eq
+  _ = (List.Ico 111 1000).sum := h_perm.sum_eq
   _ = ((List.Ico 111 1000).map id).sum := by simp
   _ = ∑ x in (List.Ico 111 1000).toFinset, x := by
     refine (List.sum_toFinset _ ?_).symm
@@ -58,7 +59,6 @@ theorem number_theory_199764 (l : List ℕ) (hl : l ~ List.Ico 111 1000) :
     ext x
     simp
   _ = ∑ x ∈ Finset.range 889, (111 + x) := by rw [Finset.sum_Ico_eq_sum_range]
-  -- _ = 889 * 111 + 889 * (889 - 1) / 2 := by
   _ = (111 + 999) * 889 / 2 := by
     rw [Finset.sum_add_distrib]
     simp [Finset.sum_range_id]
