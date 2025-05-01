45800407bf321fbdc87176d36ea3527f537ef7f7
Jack Valmadre <jack.valmadre@gmail.com>
2025-05-01 22:10:00 +0300

    problem_265535: clean up

--- Numina/problem_265535.lean
+++ Numina/problem_265535.lean
@@ -33,15 +33,17 @@ theorem algebra_265535 (f : ℕ → ℕ) (h_mono : StrictMono f) (hf : ∀ k, f 
     _ = f (f (f k)) := congrArg f (hf k).symm
     _ = _ := hf (f k)
   -- This enables us to obtain several more values of `f`.
+  -- In order to obtain `f 96 = f (3 * 32) = 3 * f 32` we require `f 32`.
   have h3 : f 3 = 6 := by simpa [h1] using h3k 1
   have h6 : f 6 = 9 := by simpa [h2] using h3k 2
   have h9 : f 9 = 18 := by simpa [h3] using h3k 3
   have h18 : f 18 = 27 := by simpa [h6] using h3k 6
   have h27 : f 27 = 54 := by simpa [h9] using h3k 9
   have h54 : f 54 = 81 := by simpa [h18] using h3k 18
-  -- Observe that `f 54 - f 27 = 27 = 81 - 54`. By strict monotonicity, this means that
-  -- `f (27 + i) = 54 + i` for `i = 0, 1, ..., 27`.
-  -- This will enable us to obtain `f 96 = 3 * f 32`.
+
+  -- Observe that `f 54 - f 27 = 27 = 81 - 54`.
+  -- Since the function is *strictly* increasing, it must increase in steps of 1.
+  -- Hence `f (27 + i) = 54 + i` for `0 ≤ i ≤ 27`, sufficient to conclude the proof.
   suffices ∀ i < 28, f (27 + i) = 54 + i by simp [h1, h9, h3k 32, this 5]
   -- Switch to `Fin 28` to use `StrictMono.range_inj`, which is defined in terms of `Set.range`.
   suffices ∀ (i : Fin 28), f (27 + i.val) = 54 + i.val by simpa [Fin.forall_iff] using this
@@ -49,30 +51,19 @@ theorem algebra_265535 (f : ℕ → ℕ) (h_mono : StrictMono f) (hf : ∀ k, f 
   refine (StrictMono.range_inj ?_ ?_).mp ?_
   · exact h_mono.comp (Fin.val_strictMono.const_add 27)
   · exact Fin.val_strictMono.const_add 54
-  calc _
-  _ = Set.range (f ∘ (27 + ·) ∘ Fin.val : Fin 28 → ℕ) := rfl
-  _ = Finset.image f (Finset.map (addLeftEmbedding 27)
-      (Finset.map Fin.valEmbedding (Finset.univ : Finset (Fin 28)))) := by
-    simp [Set.range_comp]
-  _ = Finset.image f (Finset.map (addLeftEmbedding 27) (Finset.Iio 28)) := by
-    rw [Fin.map_valEmbedding_univ]
-  _ = Finset.image f (Finset.map (addLeftEmbedding 27) (Finset.Ico 0 28)) := by simp
-  _ = Finset.image f (Finset.Ico 27 55) := by rw [Finset.map_add_left_Ico]
-  _ = Finset.image f (Finset.Icc 27 54) := rfl
-  _ = Finset.Icc (f 27 ) (f 54) := by
-    refine congrArg _ ?_
-    refine Finset.eq_of_subset_of_card_le ?_ ?_
-    · suffices Finset.toSet (Finset.image f (Finset.Icc 27 54)) ⊆
-          Finset.toSet (Finset.Icc (f 27) (f 54)) from this
-      simp only [Finset.coe_image, Finset.coe_Icc]
-      exact h_mono.monotone.image_Icc_subset
-    · refine le_of_eq ?_
-      calc _
-      _ = (Finset.Icc 27 54).card := by simp [h27, h54]
-      _ = _ := (Finset.card_image_of_injective _ h_mono.injective).symm
-  _ = _ := by
-    rw [h27, h54]
-    calc _
-    _ = Finset.toSet (.map (addLeftEmbedding 54)
-        (.map Fin.valEmbedding (.univ : Finset (Fin 28)))) := rfl
-    _ = Set.range ((54 + ·) ∘ fun x : Fin 28 ↦ x.val) := by simp [Set.range_comp]
+
+  -- Now it remains to prove that the two sets are equal using their cardinality.
+  -- Switch back to `Finset ℕ` where finiteness is known and finite intervals have better support.
+  suffices Finset.univ.image (f ∘ (27 + ·) ∘ Fin.val : Fin 28 → ℕ) =
+      Finset.univ.image ((54 + ·) ∘ Fin.val : Fin 28 → ℕ) by
+    simpa using congrArg Finset.toSet this
+  -- Definitionally equal to `Finset.Icc`.
+  change (Finset.Icc 27 54).image f = Finset.Icc 54 81
+  refine Finset.eq_of_subset_of_card_le ?_ ?_
+  · -- Replace `54` and `81` with `f 27` and `f 54`.
+    suffices (Finset.Icc 27 54).image f ⊆ Finset.Icc (f 27) (f 54) by simpa [h27, h54] using this
+    -- Switch to `Set ℕ` in order to use `Monotone.image_Icc_subset`.
+    refine Finset.coe_subset.mp ?_
+    simpa using h_mono.monotone.image_Icc_subset
+  · -- Use injectivity of `f` (from strict monotonicity) to show cardinalities are equal.
+    simp [Finset.card_image_of_injective _ h_mono.injective]
