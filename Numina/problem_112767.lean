389c32215ea73cbb361385a7f9b6de76867aea63
Jack Valmadre <jack.valmadre@gmail.com>
2025-05-13 07:17:38 +0300

    problem_112767: complete

--- Numina/problem_112767.lean
+++ Numina/problem_112767.lean
@@ -13,17 +13,17 @@ $$
 theorem inequalities_112767 (a b c : ℝ) (ha : 0 < a) (hb : 0 < b) (hc : 0 < c)
     (habc : a * b * c = 1) :
     1 / (a + b) + 1 / (b + c) + 1 / (a + c) ≤ (a ^ 2 + b ^ 2 + c ^ 2) / 2 := by
-  -- The AM-HM inequality provides `(a + b) / 2 ≥ 1 / (1 / (a + b)) = 2 * a * b / (a + b)`
+  -- The AM-HM inequality provides `(a + b) / 2 ≥ 2 * a * b / (a + b)`
   -- and hence `1 / (a + b) ≤ (a + b) / (4 * a * b) = c * (a + b) / 4`.
+  -- First combine the AM-GM and GM-HM inequalities to obtain the AM-HM inequality.
   have h_hm_am {u v : ℝ} (hu : 0 < u) (hv : 0 < v) :
       2 * (u * v) / (u + v) ≤ (u + v) / 2 := by
     convert le_trans
       (harm_mean_le_geom_mean Finset.univ (by simp) ![1, 1] ![u, v]
-        (by simp [Fin.forall_fin_two]) (by simp)
-        (by simpa [Fin.forall_fin_two] using ⟨hu, hv⟩))
+        (by simp [Fin.forall_fin_two]) (by simp) (by simp [Fin.forall_fin_two, hu, hv]))
       (geom_mean_le_arith_mean (ι := Fin 2) Finset.univ ![1, 1] ![u, v]
-        (by simp [Fin.forall_fin_two]) (by simp)
-        (by simpa [Fin.forall_fin_two] using ⟨hu.le, hv.le⟩)) using 1
+        (by simp [Fin.forall_fin_two]) (by simp) (by simp [Fin.forall_fin_two, hu.le, hv.le]))
+      using 1
     · symm
       calc _
       _ = (1 + 1) / (u⁻¹ + v⁻¹) := by simp
@@ -43,30 +43,32 @@ theorem inequalities_112767 (a b c : ℝ) (ha : 0 < a) (hb : 0 < b) (hc : 0 < c)
       _ = (u + v) / (1 + 1) := by simp
       _ = _ := by norm_num
 
+  -- Now re-arrange the terms and use `a b c = 1`.
   have h_hm_am' {u v w : ℝ} (hu : 0 < u) (hv : 0 < v) (hw : 0 < w) (huvw : u * v * w = 1) :
       1 / (u + v) ≤ (u + v) * w / 4 := by
-    suffices 1 / (u + v) ≤ (u + v) * w * (u * v) / (4 * (u * v)) by
+    -- Replace `w` with `1 / (u * v)`.
+    suffices 1 / (u + v) ≤ (u + v) / (4 * (u * v)) by
       convert this using 1
-      refine (mul_div_mul_right _ _ ?_).symm
-      exact mul_ne_zero hu.ne' hv.ne'
-    -- TODO: merge with above and use `calc`?
-    suffices 1 / (u + v) ≤ (u + v) * (u * v * w) / (4 * (u * v)) by
-      convert this using 2
-      ring
-    suffices 1 / (u + v) ≤ (u + v) / (4 * (u * v)) by simpa [huvw] using this
-    suffices 4 * (u * v) ≤ (u + v) * (u + v) by
-      refine (div_le_div_iff₀ ?_ ?_).mpr ?_
+      calc _
+      _ = (u + v) * w * (u * v) / (4 * (u * v)) := by
+        refine (mul_div_mul_right _ _ ?_).symm
+        exact mul_ne_zero hu.ne' hv.ne'
+      _ = (u + v) * (u * v * w) / (4 * (u * v)) := by ring
+      _ = _ := by simp [huvw]
+    -- Multiply both sides by 2.
+    suffices 2 / (u + v) ≤ (u + v) / (2 * (u * v)) by
+      refine (mul_le_mul_right two_pos).mp ?_
+      convert this using 1 <;> ring
+    -- Take the inverse of both sides.
+    refine (inv_le_inv₀ ?_ ?_).mp ?_
+    · refine div_pos ?_ (mul_pos two_pos ?_)
       · exact add_pos hu hv
-      · exact mul_pos four_pos (mul_pos hu hv)
-      convert this using 1
-      ring
-    suffices 2 * (u * v) / (u + v) ≤ (u + v) / 2 by
-      convert (div_le_div_iff₀ ?_ two_pos).mp this using 1
-      · ring
-      · exact add_pos hu hv
-    exact h_hm_am hu hv
+      · exact mul_pos hu hv
+    · exact div_pos two_pos (add_pos hu hv)
+    convert h_hm_am hu hv using 1 <;> simp
 
   calc _
+  -- Apply our inequality to each term.
   _ ≤ (a + b) * c / 4 + (b + c) * a / 4 + (c + a) * b / 4 := by
     refine add_le_add (add_le_add ?_ ?_) ?_
     · exact h_hm_am' ha hb hc habc
@@ -77,10 +79,13 @@ theorem inequalities_112767 (a b c : ℝ) (ha : 0 < a) (hb : 0 < b) (hc : 0 < c)
       refine h_hm_am' hc ha hb ?_
       convert habc using 1
       ring
+  -- Expand and gather terms.
   _ = (a * b + b * c + c * a) / 2 := by ring
   _ ≤ _ := by
+    -- Rewrite as a non-negative difference.
     refine div_le_div_of_nonneg_right ?_ two_pos.le
     refine sub_nonneg.mp ?_
+    -- Show that this difference is equal to a sum of squares.
     calc _
     _ ≤ ((a - b) ^ 2 + (b - c) ^ 2 + (c - a) ^ 2) / 2 := by
       refine div_nonneg ?_ two_pos.le
