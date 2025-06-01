4855cd5e80c1284f53973c0794a464c657d33d67
Jack Valmadre <jack.valmadre@gmail.com>
2025-06-01 20:12:51 +0200

    problem_111609: tweaks

--- Numina/problem_111609.lean
+++ Numina/problem_111609.lean
@@ -33,12 +33,11 @@ theorem algebra_111609 {a b c d : ℝ} (ha : a ≠ 0)
   have ⟨ha, hc⟩ : a = 1 ∧ c = -3 := by
     simp only [hf, hb, hd, eval_add, eval_mul, eval_C, eval_pow, eval_X] at hf_0800 hf_1300
     constructor <;> nlinarith
-
   refine ⟨⟨ha, hb, hc, hd⟩, ?_⟩
 
   replace hf : f = X ^ 3 - C 3 * X + C 60 := by simpa [ha, hb, hc, hd] using hf
-  -- The derivative of `x ^ 3 - 3 x + c` is is `3 (x ^ 2 - 1)`.
-  -- This is zero at `x = -1` and `x = 1`, negative in `(-1, 1)`, and positive elsewhere.
+  -- The derivative of `x ^ 3 - 3 x + c` is is `3 (x ^ 2 - 1)`,
+  -- which is zero at `x = -1` and `x = 1`, negative in `(-1, 1)`, positive elsewhere.
   have hf' : deriv f.eval = (C 3 * (X ^ 2 - C 1) : ℝ[X]).eval := by
     funext x
     simpa [hf, mul_sub] using f.deriv
@@ -46,6 +45,7 @@ theorem algebra_111609 {a b c d : ℝ} (ha : a ≠ 0)
   -- The second derivative is `6 x`, which is negative for `x < 0` and positive for `x > 0`.
   -- Therefore, we have a strictly concave function on `x < 0` with maximum at `x = -1`,
   -- and a strictly convex function on `0 < x`.
+  -- The maximum is obtained at both `x = -1` and at the boundary `x = 2`.
   have hf'' : deriv^[2] f.eval = (C 6 * X : ℝ[X]).eval := by
     simp only [Function.iterate_succ, Function.comp_apply, hf']
     funext x
@@ -61,7 +61,8 @@ theorem algebra_111609 {a b c d : ℝ} (ha : a ≠ 0)
     intro x hx
     simpa [hf''] using hx
 
-  -- Obtain IsMaxOn for `-2 ≤ x ≤ 0`. This will be combined with strict concavity for uniqueness.
+  -- Obtain `IsMaxOn` for `-2 ≤ x ≤ 0`.
+  -- This will be combined with strict concavity to obtain uniqueness of the maximum.
   have h_isMaxOn_neg_one : IsMaxOn f.eval (Set.Icc (-2) 0) (-1) := by
     suffices IsMaxOn f.eval (Set.Iic 0) (-1) from this.on_subset Set.Icc_subset_Iic_self
     refine IsMaxOn.of_isLocalMaxOn_of_concaveOn (by norm_num) ?_ h_concave.concaveOn
@@ -83,45 +84,46 @@ theorem algebra_111609 {a b c d : ℝ} (ha : a ≠ 0)
       rw [abs_le]
       constructor <;> linarith
 
-  -- Use this to split the interval as needed.
+  -- It will be useful to split the interval.
   have h_union : Set.Icc (-2) 0 ∪ Set.Ioc 0 2 = (Set.Icc (-2) 2 : Set ℝ) :=
     Set.Icc_union_Ioc_eq_Icc (by simp) (by simp)
 
   refine ⟨?_, ?_⟩
+  -- First show that the function is bounded above on the interval.
   · intro x hx
-    -- Split into two cases.
+    -- Split into the two cases.
     rw [← h_union, Set.mem_union] at hx
     cases hx with
     | inl hx =>
-      -- Use concavity and first, second derivatives to show that maximum is attained at (-1).
+      -- For `-2 ≤ x ≤ 0`, bound above by `f (-1)`.
       calc _
       _ ≤ f.eval (-1) := h_isMaxOn_neg_one hx
       _ = _ := by norm_num [hf]  -- TODO: this everywhere
     | inr hx =>
-      -- Use convexity to show that max is attained at an endpoint.
+      -- For `0 < x ≤ 2`, bound above by `f 2`.
       calc _
       _ ≤ max (f.eval 0) (f.eval 2) :=
         h_convex.convexOn.le_max_of_mem_Icc (by simp) (by simp) (Set.mem_Icc_of_Ioc hx)
       _ = _ := by norm_num [hf]
-
+  -- Then it remains to show that no other points obtain the maximum.
   · ext x
     simp only [Set.mem_setOf_eq, Set.mem_insert_iff, Set.mem_singleton_iff]
     constructor
     · intro ⟨hx, h⟩
+      -- Show that each maximum is unique within its interval.
       rw [← h_union, Set.mem_union] at hx
       refine hx.imp ?_ ?_
       · intro hx
-        -- Use strict concavity combined with (-1) being maximizer to show uniqueness.
+        -- Use strict concavity to obtain uniqueness of the maximizer.
         replace h_concave := h_concave.subset (Set.Icc_subset_Iic_self) (convex_Icc (-2) 0)
-        refine h_concave.eq_of_isMaxOn ?_ ?_ hx (by norm_num)
-        · intro a ha
-          simp only [Set.mem_setOf_eq]
-          calc _
-          _ ≤ f.eval (-1) := h_isMaxOn_neg_one ha
-          _ = 62 := by norm_num [hf]
-          _ = _ := h.symm
-        · exact h_isMaxOn_neg_one
-      · -- Use strict convexity to show strictly less than the max endpoint.
+        refine h_concave.eq_of_isMaxOn ?_ h_isMaxOn_neg_one hx (by norm_num)
+        intro a ha
+        simp only [Set.mem_setOf_eq]
+        calc _
+        _ ≤ f.eval (-1) := h_isMaxOn_neg_one ha
+        _ = 62 := by norm_num [hf]
+        _ = _ := h.symm
+      · -- Use strict convexity to show other points are strictly less than the max endpoint.
         suffices x ∉ Set.Ioo 0 2 by
           intro hx
           simpa using Set.mem_diff_of_mem hx this
@@ -131,7 +133,7 @@ theorem algebra_111609 {a b c d : ℝ} (ha : a ≠ 0)
         _ < eval 0 f ⊔ eval 2 f :=
           h_convex.lt_on_openSegment (Set.left_mem_Ici) (by simp) (by simp) (by simpa using h)
         _ = 62 := by norm_num [hf]
-
+    -- The reverse direction is simple evaluation.
     · intro hx
       refine hx.elim ?_ ?_
       · rintro rfl
